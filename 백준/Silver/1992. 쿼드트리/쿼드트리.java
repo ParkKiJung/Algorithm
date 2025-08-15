1d2e1f2048bc59d7d19928103e1ac74a1c5032ff
ParkKiJung <105425330+ParkKiJung@users.noreply.github.com>
2025-08-15 21:15:21 +0900

    [Silver I] Title: 쿼드트리, Time: 68 ms, Memory: 11748 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,46 +1,42 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    static int[][] location;
-    static String output = "";
-    static void findArea(int startRow, int startCol, int size) {
 
-        int curArea = location[startRow][startCol];
-        for (int i = startRow; i < startRow + size; i++) {
-            for (int j = startCol; j < startCol + size; j++) {
-                if (location[i][j] != curArea) {
-                    int newSize = size / 2;
-                    output += "(";
-                    findArea(startRow, startCol, newSize);
-                    findArea(startRow, startCol + newSize, newSize);
-                    findArea(startRow + newSize, startCol, newSize);
-                    findArea(startRow + newSize, startCol + newSize, newSize);
-                    output += ")";
+    static StringBuilder sb = new StringBuilder();
+    static char[][] map;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        find(0, 0, N, map[0][0]);
+
+        System.out.println(sb.toString());
+    }
+
+    static void find(int startRow, int startCol, int size, char val) {
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (map[startRow + i][startCol + j] != val) {
+                    sb.append("(");
+                    find(startRow, startCol, size / 2, map[startRow][startCol]);
+                    find(startRow, startCol + size / 2, size / 2, map[startRow][startCol + size / 2]);
+                    find(startRow + size / 2, startCol, size / 2, map[startRow + size / 2][startCol]);
+                    find(startRow + size / 2, startCol + size / 2, size / 2, map[startRow + size / 2][startCol + size / 2]);
+                    sb.append(")");
                     return;
                 }
             }
         }
 
-        output += Integer.toString(curArea);
-
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int size = Integer.parseInt(br.readLine());
-        location = new int[size][size];
-
-        for (int i = 0; i < size; i++) {
-            String[] input = br.readLine().split("");
-            for (int j = 0; j < size; j++) {
-                location[i][j] = Integer.parseInt(input[j]);
-            }
-        }
-
-        findArea(0, 0, size);
-        System.out.println(output);
+        sb.append(val);
 
     }
 }
