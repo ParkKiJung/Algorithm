b8eaa243c073fabddc0835fcc30ea75382ecf93c
ParkKiJung <105425330+ParkKiJung@users.noreply.github.com>
2025-10-22 15:58:42 +0900

    [Gold IV] Title: 무기 공학, Time: 264 ms, Memory: 18648 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.java
+++ 백준/Gold/18430. 무기 공학/무기 공학.java
@@ -6,7 +6,8 @@ public class Main {
     static int N, M, answer = 0;
     static int[][] map;
     static boolean[][] used;
-    static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // 상우하좌
+    // 상우하좌 (시계방향)
+    static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,43 +31,51 @@ public class Main {
         System.out.println(answer);
     }
 
-    static void find(int sum, int lastRow) {
+    static void find(int idx, int sum) {
         answer = Math.max(answer, sum);
+        if (idx == N * M) {
+            return;
+        }
 
-        for (int i = lastRow; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (used[i][j]) continue;
+        int r = idx / M;
+        int c = idx % M;
 
-                // 부메랑 모양별로 확인하기
-                for (int k = 0; k < 4; k++) {
-                    int nR1 = i + dir[k][0];
-                    int nC1 = j + dir[k][1];
-                    // 사용할 모양 중 한쪽이 사용중이거나 범위를 넘어갈 때
-                    if (nR1 >= N || nR1 < 0 || nC1 >= M || nC1 < 0 || used[nR1][nC1]) {
-                        continue;
-                    }
+        if (used[r][c]) {
+            // 사용된 경우 다음 칸으로 이동
+            find(idx + 1, sum);
+            return;
+        }
 
-                    int dIDx = (k + 1) % 4;
-                    int nR2 = i + dir[dIDx][0];
-                    int nC2 = j + dir[dIDx][1];
-                    if (nR2 >= N || nR2 < 0 || nC2 >= M || nC2 < 0 || used[nR2][nC2]) {
-                        continue;
-                    }
+        find(idx + 1, sum);
 
-                    used[nR1][nC1] = true;
-                    used[nR2][nC2] = true;
-                    used[i][j] = true;
-                    int nextSum = sum + map[nR1][nC1] + map[nR2][nC2] + (map[i][j] * 2);
+        for (int k = 0; k < 4; k++) {
 
-                    find(nextSum, i);
+            int nR1 = r + dir[k][0];
+            int nC1 = c + dir[k][1];
 
-                    used[nR1][nC1] = false;
-                    used[nR2][nC2] = false;
-                    used[i][j] = false;
-                }
+            int dIDx = (k + 1) % 4;
+            int nR2 = r + dir[dIDx][0];
+            int nC2 = c + dir[dIDx][1];
 
+            if (isValid(nR1, nC1) && isValid(nR2, nC2) && !used[nR1][nC1] && !used[nR2][nC2]) {
+
+                used[r][c] = true;
+                used[nR1][nC1] = true;
+                used[nR2][nC2] = true;
+
+                int nextSum = sum + map[nR1][nC1] + map[nR2][nC2] + (map[r][c] * 2);
+
+                // 다음 칸으로 이동
+                find(idx + 1, nextSum);
+
+                used[r][c] = false;
+                used[nR1][nC1] = false;
+                used[nR2][nC2] = false;
             }
         }
     }
 
+    static boolean isValid(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < M;
+    }
 }
